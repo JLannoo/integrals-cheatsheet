f1ead2d34330d5f78c183f469889ffe150962319
JLannoo <julianlannoo@gmail.com>
2022-08-18 00:44:45 -0300

    Add title

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,10 @@
+import "./Title.css";
+
+export default function Title(){
+    return (
+        <div className="title">
+            <h1 >Integrals Cheatsheet</h1>
+            <h2>A cheatsheet for commonly used integrals</h2>
+        </div>
+    )
+}
