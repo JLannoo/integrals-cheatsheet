2461545cc314bc423e69c06a0a9357dbec79d9af
JLannoo <julianlannoo@gmail.com>
2022-09-20 09:03:09 -0300

    Configure build for new domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: "https://jlannoo.github.io",
-  base: "/integrals-cheatsheet",
+  site: "https://integrals.jlannoo.dev",
 });
